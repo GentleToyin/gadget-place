13683eeeadfd884be1e8893c8e4fe9e33d8d37c9
GentleToyin <41758364+GentleToyin@users.noreply.github.com>
2021-11-25 13:23:50 +0100

    Product Details Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -12,10 +12,10 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-          <Routes>
-            <Route path='/' element={<HomeScreen />} exact />
-            <Route path='/product/:id' element={<ProductScreen />} />
-          </Routes>
+
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
+
         </Container>
       </main>
       <Footer />
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { LinkContainer } from 'react-router-bootstrap'
 import { Navbar, Nav, Container } from 'react-bootstrap';
 
 const Header = () => {
@@ -7,18 +7,18 @@ const Header = () => {
         <header>
             <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
                 <Container>
-                    <Link to={'/'}>
-                        <Navbar.Brand href="/">Gadget Place</Navbar.Brand>
-                    </Link>
+                    <LinkContainer to={'/'}>
+                        <Navbar.Brand>Gadget Place</Navbar.Brand>
+                    </LinkContainer>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ml-auto">
-                            <Link to={'/cart'}>
-                                <Nav.Link href="/cart"><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
-                            </Link>
-                            <Link to={'/login'}>
-                                <Nav.Link href="/login"><i className="fas fa-user"></i>Sign In</Nav.Link>
-                            </Link>
+                            <LinkContainer to={'/cart'}>
+                                <Nav.Link><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to={'/login'}>
+                                <Nav.Link><i className="fas fa-user"></i>Sign In</Nav.Link>
+                            </LinkContainer>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
